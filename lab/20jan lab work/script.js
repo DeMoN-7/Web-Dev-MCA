24d73fb8e06ff83cf3ac2813f8b333a68bb415db
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-01-20 14:22:18 +0530

    commit

---
+++ lab/20jan lab work/script.js
@@ -0,0 +1,11 @@
+const gender=prompt("Enter you gender")
+const age=confirm("Over 60 or not")
+if (age){
+    console.log("hello ji")
+    alert("Adult")
+}
+else{
+    alert("Under 18")
+    console.log("hello ji")
+}
+document.write("your gender:",gender)
