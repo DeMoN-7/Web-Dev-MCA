d9ce596b2d6b616a55a578a5eee691758efe7fcf
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-02-04 08:57:18 +0530

    commit

---
+++ NodeJs/4feb/server.js
@@ -0,0 +1,8 @@
+const fs = require('fs'); 
+fs.readFile('example.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+  console.log('File contents:\n', data);
+});
