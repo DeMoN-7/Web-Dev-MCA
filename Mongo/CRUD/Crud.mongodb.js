9fe3888c0bcda810598a6ce6468c33f1fe444467
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-03-10 01:21:49 +0530

    commmit

---
+++ Mongo/CRUD/Crud.mongodb.js
@@ -0,0 +1,19 @@
+// db.createCollection("SalesData");
+
+
+// inserting many data together using insert many function
+// db.SalesData.insertMany([
+//     { 'item': 'iPhone 12', 'price': 799, 'quantity': 2, 'date': new Date('2021-03-01T08:00:00Z') },
+//   { 'item': 'Samsung Galaxy S21', 'price': 999, 'quantity': 1, 'date': new Date('2021-03-05T09:00:00Z') },
+//   { 'item': 'MacBook Pro 16', 'price': 2399, 'quantity': 1, 'date': new Date('2021-04-10T09:30:00Z') },
+//   { 'item': 'Sony WH-1000XM4', 'price': 349, 'quantity': 4, 'date': new Date('2021-06-15T11:00:00Z') },
+//   { 'item': 'Apple Watch Series 7', 'price': 429, 'quantity': 3, 'date': new Date('2021-09-20T14:45:00Z') },
+//   { 'item': 'Dell XPS 13', 'price': 1299, 'quantity': 2, 'date': new Date('2022-01-12T16:10:00Z') },
+//   { 'item': 'PlayStation 5', 'price': 499, 'quantity': 5, 'date': new Date('2022-02-22T10:00:00Z') },
+//   { 'item': 'Xbox Series X', 'price': 499, 'quantity': 6, 'date': new Date('2022-03-05T12:30:00Z') },
+  
+// ]);
+
+// reading data using find function
+
+// db.SalesData.find({price:499});
