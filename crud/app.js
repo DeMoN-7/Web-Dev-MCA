1a925908194110277338b4518312a6f40e18a102
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-03-17 22:54:01 +0530

    commit

--- crud/app.js
+++ crud/app.js
@@ -1,6 +1,32 @@
-const express= require('express');
-const app=express();
-app.get('/',(req,res)=>{
-    res.send("heyff");
-})
+const express = require("express");
+const app = express();
+const studentModel = require("./studentsModel");
+
+app.get("/", (req, res) => {
+  res.send("heyff");
+});
+app.get("/create", async (req, res) => {
+  let createdStudent = await studentModel.create({
+    name: "Aman Choudhary",
+    course: "MCA",
+    specialization: "AI",
+  });
+
+  res.send(createdStudent);
+});
+app.get("/update", async (req, res) => {
+  let UpdateStudent = await studentModel.findOneAndUpdate(
+    { name: "Ayush" },
+    { name: "Ayush Singh" },
+    { new: true }
+  );
+
+  res.send(UpdateStudent);
+});
+
+app.get("/read", async (req, res) => {
+  let ReadStudent = await studentModel.find();
+  res.send(ReadStudent);
+});
+
 app.listen(3000);
